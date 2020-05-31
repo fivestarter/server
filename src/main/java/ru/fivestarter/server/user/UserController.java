289397d3aebd9b363d58d27e6def41bc1c2afcb7
yury <fivestarter@gmail.com>
2020-05-31 16:11:28 +0300

    get user was added

--- src/main/java/ru/fivestarter/server/user/UserController.java
+++ src/main/java/ru/fivestarter/server/user/UserController.java
@@ -1,4 +1,20 @@
 package ru.fivestarter.server.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    @GetMapping
+    public ResponseEntity<UserJson> getUser() {
+        UserJson user = new UserJson();
+        user.setFirstName("firstName");
+        user.setLastName("lastName");
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
