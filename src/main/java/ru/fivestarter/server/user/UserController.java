5f9a2d822a2ef2f2263278fa16006a81929956dc
fivestarter <fivestarter@gmail.com>
2020-06-04 22:27:13 +0300

    cors

--- src/main/java/ru/fivestarter/server/user/UserController.java
+++ src/main/java/ru/fivestarter/server/user/UserController.java
@@ -2,10 +2,12 @@ package ru.fivestarter.server.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:63342", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
@@ -13,8 +15,8 @@ public class UserController {
     @GetMapping
     public ResponseEntity<UserJson> getUser() {
         UserJson user = new UserJson();
-        user.setFirstName("firstName");
-        user.setLastName("lastName");
+        user.setFirstName("Mick");
+        user.setLastName("Jagger");
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
